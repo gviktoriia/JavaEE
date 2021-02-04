cb8d004b93a474dbc02d0ca1bc516d66d8508aa2
Oleh Kurpiak <oleh.kurpiak@gmail.com>
2021-02-04 08:27:42 +0200

    clean-up imports

--- topic5/samples/spring-web-sample/src/main/java/kma/topic5/springwebsample/FormController.java
+++ topic5/samples/spring-web-sample/src/main/java/kma/topic5/springwebsample/FormController.java
@@ -3,12 +3,8 @@ package kma.topic5.springwebsample;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
-import lombok.Getter;
-import lombok.Setter;
 
 @Controller
 public class FormController {
--- topic5/samples/spring-web-sample/src/main/java/kma/topic5/springwebsample/FormModel.java
+++ topic5/samples/spring-web-sample/src/main/java/kma/topic5/springwebsample/FormModel.java
@@ -1,6 +1,5 @@
 package kma.topic5.springwebsample;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
