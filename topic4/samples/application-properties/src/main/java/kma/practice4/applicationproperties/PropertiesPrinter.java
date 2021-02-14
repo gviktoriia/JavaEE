219fd4b31c0adfd3e607be7b763da661a8b165b7
Oleh Kurpiak <oleh.kurpiak@gmail.com>
2021-02-14 09:53:13 +0200

    application properties example

--- topic4/samples/application-properties/src/main/java/kma/practice4/applicationproperties/PropertiesPrinter.java
+++ topic4/samples/application-properties/src/main/java/kma/practice4/applicationproperties/PropertiesPrinter.java
@@ -3,13 +3,16 @@ package kma.practice4.applicationproperties;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import lombok.RequiredArgsConstructor;
+
 @Component
+@RequiredArgsConstructor
 public class PropertiesPrinter {
 
     @Value("${my.first.property}")
-    private String firstProperty;
+    private final String firstProperty;
     @Value("${my.second.property}")
-    private String secondProperty;
+    private final String secondProperty;
 
     public void printInfo() {
         System.out.println("First property: " + firstProperty);
